ae83dc174297c950b785ba99ba3ca3d0eb38820b
RianLL15 <152223711+RianLL15@users.noreply.github.com>
2025-03-23 12:51:33 -0300

    Update Contando_numeros_de_0_a_9_com_o_botao.ino

--- Projetos/Display de sete segmentos/Contador com o botão/Contando_numeros_de_0_a_9_com_o_botao/Contando_numeros_de_0_a_9_com_o_botao.ino
+++ Projetos/Display de sete segmentos/Contador com o botão/Contando_numeros_de_0_a_9_com_o_botao/Contando_numeros_de_0_a_9_com_o_botao.ino
@@ -2,7 +2,7 @@
 
 // Pinos dos segmentos para cada display (unidades)
 const int segmentPins[3][7] = {
-  {22, 23, 24, 25, 26, 27, 28}, // Unidades
+  {3, 4, 5, 6, 7, 8, 9}, // Unidades
 };
 
 int counter = 0;
